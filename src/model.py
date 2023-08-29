8ba3ffc8fbac6164048c8d63285fc46983a7426e
kimjinho1 <rlawlsgh8113@naver.com>
2023-08-30 02:53:09 +0900

    feat: Video 모델 생성

--- src/model.py
+++ src/model.py
@@ -1,4 +1,13 @@
-from sqlalchemy import Column, String, Integer, TIMESTAMP, UniqueConstraint
+from sqlalchemy import (
+    Column,
+    String,
+    Integer,
+    Boolean,
+    TIMESTAMP,
+    ForeignKey,
+    UniqueConstraint,
+)
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -26,3 +35,24 @@ class Channel(Base):
             "vtuber_name", "channel_name", "custom_url", "youtube_api_channel_id"
         ),
     )
+
+
+# 종료된 라이브 유튜브 영상 정보
+class Video(Base):
+    __tablename__ = "video"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(255), nullable=False)
+    video_url = Column(String(255), nullable=False)
+    thumbnail_url = Column(String(255), nullable=False)
+    is_live = Column(Boolean, nullable=False)
+    view_count = Column(Integer, nullable=False)
+    like_count = Column(Integer, nullable=False)
+    comment_count = Column(Integer, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False)
+    youtube_api_video_id = Column(String(50), nullable=False)
+
+    channel_id = Column(Integer, ForeignKey("channel.id"), nullable=False)
+    channel = relationship("Channel")
+
+    __table_args__ = (UniqueConstraint("video_url", "youtube_api_video_id"),)
